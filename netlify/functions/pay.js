1d504cdcbd36eaa119e0e1a895ecc8f70cab5023
yseokcha-beep <yseokcha@gmail.com>
2026-05-06 04:44:32 -0400

    Update pay.js

--- netlify/functions/pay.js
+++ netlify/functions/pay.js
@@ -1,9 +1,22 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 exports.handler = async (event) => {
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+      body: '',
+    };
+  }
+
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Method Not Allowed' };
   }
+
   try {
     const { amount, bar, staffName, staffNo, fromName, message } = JSON.parse(event.body);
 
@@ -28,21 +41,23 @@ exports.handler = async (event) => {
         staff_no: staffNo || '',
         from: fromName || 'Anonymous',
         message: message || '',
-        tip_amount: amount,
-        fee_amount: fee,
+        tip_amount: String(amount),
+        fee_amount: String(fee),
       }
     });
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ 
-        success: true, 
-        clientSecret: paymentIntent.client_secret 
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({
+        clientSecret: paymentIntent.client_secret
       })
     };
+
   } catch (err) {
     return {
       statusCode: 400,
+      headers: { 'Access-Control-Allow-Origin': '*' },
       body: JSON.stringify({ error: err.message })
     };
   }
